ddf47c06066a2ae5a4536074b8d971e133b541ca
RoniSegal <roni@sealihts.io>
2017-05-15 17:18:42 +0300

    export restore logic to  function

--- src/main/java/io/sealights/plugins/sealightsjenkins/buildsteps/cli/SealightsCLIBuildStep.java
+++ src/main/java/io/sealights/plugins/sealightsjenkins/buildsteps/cli/SealightsCLIBuildStep.java
@@ -41,33 +41,38 @@ public class SealightsCLIBuildStep extends Builder {
     * of this plugin.
     */
     private Object readResolve() {
-        if(cliRunner!=null) {
-            StringBuffer additionalArgs = new StringBuffer();
-            if (this.commandMode instanceof CommandMode.ConfigView) {
-                if (!StringUtils.isNullOrEmpty(cliRunner.getAppName()))
-                    ((CommandMode.ConfigView) commandMode).setAppName(cliRunner.getAppName());
-                if (!StringUtils.isNullOrEmpty(cliRunner.getBranchName()))
-                    ((CommandMode.ConfigView) commandMode).setBranchName(cliRunner.getBranchName());
-                if (cliRunner.getBuildName() != null)
-                    ((CommandMode.ConfigView) commandMode).setBuildName(cliRunner.getBuildName());
-            } else if (this.commandMode instanceof CommandMode.EndView ||
-                    this.commandMode instanceof CommandMode.StartView ||
-                    this.commandMode instanceof CommandMode.ExternalReportView ||
-                    this.commandMode instanceof CommandMode.UploadReportsView) {
-                if (!StringUtils.isNullOrEmpty(cliRunner.getAppName())) {
-                    additionalArgs.append("appName=" + cliRunner.getAppName() + "\n");
-                }
-                if (!StringUtils.isNullOrEmpty(cliRunner.getBranchName())) {
-                    additionalArgs.append("branchName=" + cliRunner.getBranchName() + "\n");
-                }
-
-            }
-            if (!StringUtils.isNullOrEmpty(cliRunner.getAdditionalArguments()))
-                additionalArgs.insert(0,cliRunner.getAdditionalArguments()+"\n");
-            (commandMode).setAdditionalArguments(additionalArgs.toString());
+        if (cliRunner != null) {
+            resolveFromCLiRunner();
         }
         return this;
     }
+
+    private void resolveFromCLiRunner () {
+        StringBuffer additionalArgs = new StringBuffer();
+        if (this.commandMode instanceof CommandMode.ConfigView) {
+            if (!StringUtils.isNullOrEmpty(cliRunner.getAppName()))
+//                ((CommandMode.ConfigView) commandMode).setAppName(cliRunner.getAppName());
+            if (!StringUtils.isNullOrEmpty(cliRunner.getBranchName()))
+                ((CommandMode.ConfigView) commandMode).setBranchName(cliRunner.getBranchName());
+            if (cliRunner.getBuildName() != null)
+                ((CommandMode.ConfigView) commandMode).setBuildName(cliRunner.getBuildName());
+        } else if (this.commandMode instanceof CommandMode.EndView ||
+                this.commandMode instanceof CommandMode.StartView ||
+                this.commandMode instanceof CommandMode.ExternalReportView ||
+                this.commandMode instanceof CommandMode.UploadReportsView) {
+            if (!StringUtils.isNullOrEmpty(cliRunner.getAppName())) {
+                additionalArgs.append("appName=" + cliRunner.getAppName() + "\n");
+            }
+            if (!StringUtils.isNullOrEmpty(cliRunner.getBranchName())) {
+                additionalArgs.append("branchName=" + cliRunner.getBranchName() + "\n");
+            }
+
+        }
+        if (!StringUtils.isNullOrEmpty(cliRunner.getAdditionalArguments()))
+            additionalArgs.insert(0,cliRunner.getAdditionalArguments()+"\n");
+        (commandMode).setAdditionalArguments(additionalArgs.toString());
+    }
+
 
     public CommandMode getCommandMode() {
         return commandMode;
