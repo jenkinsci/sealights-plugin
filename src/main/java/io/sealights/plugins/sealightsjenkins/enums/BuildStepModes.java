3657e868674e983418ea799cc18df36df4da595e
RoniSegal <roni@sealihts.io>
2017-05-16 14:22:52 +0300

    rename steps and set default to buildSessionId field

--- src/main/java/io/sealights/plugins/sealightsjenkins/enums/BuildStepModes.java
+++ src/main/java/io/sealights/plugins/sealightsjenkins/enums/BuildStepModes.java
@@ -6,12 +6,12 @@ package io.sealights.plugins.sealightsjenkins.enums;
 public enum BuildStepModes {
     Off() {
         @Override public String getDisplayName() {
-            return "Disable this build step - FOR DEBUGGING PURPOSES ONLY";
+            return "Disable this step";
         }
     },
     InvokeMavenCommand() {
         @Override public String getDisplayName() {
-            return "Invoke top-level Maven targets (Sealights is disabled) - FOR DEBUGGING PURPOSES ONLY";
+            return "Invoke top-level Maven targets without SeaLights";
         }
     },
     InvokeMavenCommandWithSealights() {
@@ -21,7 +21,7 @@ public enum BuildStepModes {
     },
     PrepareSealights() {
         @Override public String getDisplayName() {
-            return "Integrate Sealights into POM files. Requires adding a 'SeaLights Continuous Testing - Cleanup' build step";
+            return "Integrate Sealights into POM files";
         }
     };
 
