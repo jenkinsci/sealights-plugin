a40d77a739653883b61c1a18a46c5ed0d74b6e08
RoniSegal <roni@sealihts.io>
2017-07-30 14:25:34 +0300

    download from sl-repo using proxy (if provided)

--- src/main/java/io/sealights/plugins/sealightsjenkins/integration/upgrade/UpgradeProxy.java
+++ src/main/java/io/sealights/plugins/sealightsjenkins/integration/upgrade/UpgradeProxy.java
@@ -11,7 +11,9 @@ import io.sealights.plugins.sealightsjenkins.utils.UrlBuilder;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -30,7 +32,7 @@ public class UpgradeProxy {
         logger.info("Trying to get recommended version. Url: '" + serverUrl + "'");
         ApacheHttpClient client = new ApacheHttpClient();
         HttpResponse httpResponse = client.getJson(
-                serverUrl, upgradeConfiguration.getProxy(), upgradeConfiguration.getToken());
+                serverUrl, upgradeConfiguration.getProxy(), upgradeConfiguration.getToken(),true);
         String jsonOrServerError = StreamUtils.toString(httpResponse.getResponseStream());
         UpgradeResponse upgradeResponse = JsonSerializer.deserialize(jsonOrServerError, UpgradeResponse.class);
         return upgradeResponse;
@@ -53,9 +55,18 @@ public class UpgradeProxy {
         boolean isSuccess = true;
         logger.info("Trying to download agent from url '" + urlToAgent + "' to folder '" + destFile + "'.");
         try {
-            URL agentUrl = new URL(urlToAgent);
             File agentDestination = new File(destFile);
-            FileUtils.copyURLToFile(agentUrl, agentDestination);
+            ApacheHttpClient client = new ApacheHttpClient();
+            HttpResponse response = client.getFile(urlToAgent, upgradeConfiguration.getProxy(), upgradeConfiguration.getToken(), false);
+            InputStream responseStream = response.getResponseStream();
+            FileOutputStream agentFile = new FileOutputStream(new File(destFile));
+            int inByte;
+            while ((inByte = responseStream.read()) != -1) {
+                agentFile.write(inByte);
+            }
+
+            responseStream.close();
+            agentFile.close();
 
             if (!agentDestination.exists()) {
                 logger.error("Failed to download recommended agent.");
